a34f75494b79dbfdeb49d684d686102819e8ccf0
ushakom1 <maria.ushakova@aalto.fi>
2022-04-29 08:08:38 +0300

    Returns a list with genes with their TATA-place, start-codone and stop-codone.

--- Code/biosynth.py
+++ Code/biosynth.py
@@ -14,11 +14,40 @@ def reading_letters(fil):
             return False
     return dna
 
-def gene(dna):
+def find_gene(dna):
     #stops = ["ATT", "ATC", "ACT"]
     m = re.search(("(...)*?ATT|(...)*?ATC|(...)*?ACT"),dna)
     #print("m is in ",m.span()[1])
     return m.span()[1]
+
+def find_genes(dna):
+    genes = []
+    s = 0
+    gn = []
+    while True:
+
+        a = dna.find("TATA", s)
+        b = dna.find("TATA", a + 3)
+        c = dna.find("TAC", a + 3)
+        # print(a, b, c)
+        while c >= b and b != -1:
+            a = b
+            b = dna.find("TATA", a + 3)
+            c = dna.find("TAC", a + 3)
+            print(a, b, c)
+        e = find_gene(dna[c:]) + len (dna[:c])
+        b = e
+        gn = [a, c, e]
+        if gn not in genes:
+            genes.append(gn)
+            s = c
+        else:
+            return genes
+    #return genes
+
+
+
+
 
 
 
@@ -37,43 +66,18 @@ def gene(dna):
 
 def main():
     rna = ""
-    fil = "test_dna"#"dna_sequence"
+    fil = "test_dna_error"#"dna_sequence"
     dna = reading_letters(fil)
-    a = dna.find("TATA")
-    b = dna.find("TATA", a + 3)
-    c = dna.find("TAC", a+ 3)
-    #print(a, b, c)
-    while c >= b and b != -1:
-        a = b
-        b = dna.find("TATA", a + 3)
-        c = dna.find("TAC", a + 3)
-        #print(b, c)
+    if dna != False:
+        DNA = find_genes(dna)
+        print(DNA)
+        for i in DNA:
+            print(dna[(i[0]): (i[2])])
+            print(dna[(i[1]): (i[2])])
 
-    print("the gene starts here ",a, ",", c) # a is the TATA place, C is the start codone place
-
-    e = gene(dna[c:]) + len (dna[:c])
-
-    print(dna[c:e])
+        print(rna)
 
 
-    #if dna != False:
-     #   print("len of the dna:",len(dna))
-    #    b = dna.find("TATA")
-    #    for i in dna[(b+4)::]:
-    #        if i == "A":
-    #            rna += "U"
-    #        if i == "T":
-    #            rna += "A"
-    #        if i == "C":
-    #            rna += "G"
-    #        if i == "G":
-    #            rna += "C"
-
-
-
-    print(rna) 
-    #print(b)
-    #print("len of rna:", len(rna))
 
 
 if __name__ == "__main__":
