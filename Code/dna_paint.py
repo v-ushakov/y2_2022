c0cb58896ea7a0b4096e4c62a09ddc48fd4b50cf
ushakom1 <maria.ushakova@aalto.fi>
2022-05-04 20:18:10 +0300

    Information about aminoacids only on separate file

--- Code/dna_paint.py
+++ Code/dna_paint.py
@@ -6,32 +6,8 @@ from biosynth import reading_letters as rl
 from biosynth import find_genes
 
 import math
-
+from amino import aminos, Yellow, Pink, Green, Blue
 nucleotides = { }
-aminos = {
-    'ATA': 'Ile', 'ATC': 'Ile', 'ATT': 'Ile', 'ATG': 'Met',
-    'ACA': 'Thr', 'ACC': 'Thr', 'ACG': 'Thr', 'ACT': 'Thr',
-    'AAC': 'Asn', 'AAT': 'Asn', 'AAA': 'Lys', 'AAG': 'Lys',
-    'AGC': 'Ser', 'AGT': 'Ser', 'AGA': 'Arg', 'AGG': 'Arg',
-    'CTA': 'Leu', 'CTC': 'Leu', 'CTG': 'Leu', 'CTT': 'Leu',
-    'CCA': 'Pro', 'CCC': 'Pro', 'CCG': 'Pro', 'CCT': 'Pro',
-    'CAC': 'His', 'CAT': 'His', 'CAA': 'GLN', 'CAG': 'GLN',
-    'CGA': 'Arg', 'CGC': 'Arg', 'CGG': 'Arg', 'CGT': 'Arg',
-    'GTA': 'Val', 'GTC': 'Val', 'GTG': 'Val', 'GTT': 'Val',
-    'GCA': 'Ala', 'GCC': 'Ala', 'GCG': 'Ala', 'GCT': 'Ala',
-    'GAC': 'Asp', 'GAT': 'Asp', 'GAA': 'Glu', 'GAG': 'Glu',
-    'GGA': 'Gly', 'GGC': 'Gly', 'GGG': 'Gly', 'GGT': 'Gly',
-    'TCA': 'Ser', 'TCC': 'Ser', 'TCG': 'Ser', 'TCT': 'Ser',
-    'TTC': 'Phe', 'TTT': 'Phe', 'TTA': 'Leu', 'TTG': 'Leu',
-    'TAC': 'Tyr', 'TAT': 'Tyr', 'TAA': 'STOP', 'TAG': 'STOP',
-    'TGC': 'Cys', 'TGT': 'Cys', 'TGA': 'STOP', 'TGG': 'Trp',}
-
-Yellow = ['Phe', 'Leu', 'Ile', 'Met','Val', 'Pro', 'Ala', 'Trp', 'Gly']
-Green = ['Ser', 'Thr', 'Gln', 'Asn', 'Cys', 'Ser']
-Pink = ['Asp', 'Glu']
-Blue = ['His', 'Lys', 'Arg']
-
-
 
 class Nucleotide:
     def __init__(self, key, r, g, b, points):
