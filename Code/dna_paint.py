5856e326068fd11544e90be283160bdba06e93b0
Maria Ushakova <maria.ushakova@aalto.fi>
2022-05-04 13:50:17 +0300

    Implement the gene zoom mode.

--- Code/dna_paint.py
+++ Code/dna_paint.py
@@ -21,11 +21,6 @@ class Nucleotide:
         self.b = b
         self.gray = qGray(r, g, b)
 
-        #self.gray_color_fac = qGray(r, g, b)
-        #self.gray = QColor(self.gray_color_fac, self.gray_color_fac, self.gray_color_fac, 85)
-        #self.hgray = QColor(r, g, b, 50)
-        #self.color = QColor(r, g, b, 100)
-
         self.shape = QPolygon([QPoint(int(x), int(y)) for (x, y) in points])
         self.shape.translate(-80, -60)
         trans = QTransform()
@@ -34,25 +29,25 @@ class Nucleotide:
         self.shape.translate(0, 110)
         self.rshape.translate(35, 110)
 
-    def draw(self, painter, gr, shape):
+    def draw(self, painter, gr, upper):
         color = QColor(((100-gr)*self.r + gr*self.gray)//100,
                        ((100-gr)*self.g + gr*self.gray)//100,
                        ((100-gr)*self.b + gr*self.gray)//100, 100)
 
         painter.setPen(QPen(Qt.black, 4, Qt.SolidLine))
         painter.setBrush(QBrush(color, Qt.SolidPattern))
-        painter.drawPolygon(shape)
-        if shape == self.shape:
-            painter.drawText(shape.boundingRect().center() + QPoint(-3, 10), self.key)
-        elif shape == self.rshape:
+
+        if upper:
+            painter.drawPolygon(self.rshape)
             key = 'U' if self.key == 'T' else self.key
-            painter.drawText(shape.boundingRect().center() + QPoint(-3, -2), key)
-        # if shape == self.shape:
-        #     painter.drawText(13, 185, self.key)
-        # elif shape == self.rshape:
-        #     x = self.rshape
-        #     y = 100
-        #     painter.drawText(x, y, self.key)
+            painter.drawText(self.rshape.boundingRect().center() + QPoint(-3, -2), key)
+        else:
+            painter.drawPolygon(self.shape)
+            painter.drawText(self.shape.boundingRect().center() + QPoint(-3, 10), self.key)
+
+    def complement(self):
+        dn = {'T': 'A', 'C': 'G', 'G': 'C', 'A': 'T'}
+        return nucleotides[dn[self.key]]
 
 
 Nucleotide('A', 100,   0, 0, [(80, 60), (80, 100), (115, 100), (115, 60),
@@ -72,11 +67,21 @@ OUT = 70
 
 class DNA_view(QWidget):
 
+    M_WHOLE = 1     # the whole genome is shown
+    M_ZOOM  = 2     # zooming to a particular gene
+
     def __init__(self, dna):
         super().__init__()
         #self.setStyleSheet("background : #cbd4fb;")
         self.dna = dna
-        self.resize(36*len(self.dna), 300)
+        self.genes = find_genes(dna)
+        #-----------------------------------------------------------------------
+        self.mode = self.M_ZOOM
+        self.gene = self.genes[1]           # an item from self.genes       TODO
+        #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        self.zoom = 0                       # M_ZOOM: zooming to self.gene
+        #-----------------------------------------------------------------------
+        self.fitGenome()
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.timerShot)
@@ -92,13 +97,12 @@ class DNA_view(QWidget):
         self.counterDNA = 0
         self.stage = 1
 
-        self.genes = find_genes(dna)
+
 
 
     def setCounter(self, val):
         self.counter = val
         self.repaint()
-
 
     def setCounterDNA(self, val):
         self.counterDNA = val
@@ -122,7 +126,6 @@ class DNA_view(QWidget):
             print("timer has been stopped")
             self.counterDNA = 0
 
-
     def timerColor(self):
         self.setGray(self.color_count + 1)
         self.repaint()
@@ -130,46 +133,106 @@ class DNA_view(QWidget):
             self.color_timer.stop()
             print("color timer has been stopped")
             self.timer2.start(50)
+
+    #---------------------------------------------------------------------------
+    def fitGenome(self):
+        if self.mode == self.M_WHOLE:
+            width = 36*len(self.dna)
+        elif self.mode == self.M_ZOOM:
+            _, start, end, *_ = self.gene
+            inlen = end - start
+            outlen = len(self.dna) - inlen
+            width = 36*inlen + round(36*outlen*(100 - self.zoom)/100.0)
+        else:
+            width = 0
+        self.resize(width, 300)
+
+    def setMode(self, mode):
+        self.mode = mode        # TODO: call fitGenome etc.
+        self.repaint()
+
+    def setZoom(self, zoom):
+        self.zoom = zoom
+        self.fitGenome()
+        self.repaint()
+
+    #---------------------------------------------------------------------------
+    # TODO: Modes
+    # --------------------------------------------------------------------------
+    # 1.  Whole genome. Animations: (a) top 5'->3' strand float,
+    #     (b) fading non selected genes, (c) gene selection?
+    # 2.  Zooming to a gene.
+    def paintZoom(self, ev, painter):
+
+        _, start, end, *_ = self.gene
+        hidden = round(36*start*self.zoom/100.0)
+
+        leftmost  = (hidden + ev.rect().x())//36
+        rightmost = (hidden + ev.rect().x() + ev.rect().width() + 35)//36
+
+        painter.translate(36*leftmost - hidden, 10)
+        for pos in range(leftmost, rightmost+1):
+            grayness = 0
+            if pos < start or pos >= end:
+                grayness = min(self.zoom * 2, 100)
+
+            x = nucleotides[self.dna[pos]]
+            painter.save()
+            painter.translate(0, (self.counterDNA) * 2)
+            x.draw(painter, grayness, False)
+            painter.restore()
+            painter.save()
+            painter.translate(0, (self.counter - OUT)*2)
+            x.complement().draw(painter, grayness, True)
+            painter.restore()
+            painter.translate(36, 0)
+
+
+
+    def paintWholeGenome(self, ev, painter):
+
+        leftmost = (ev.rect().x())//36
+        rightmost = (ev.rect().x() + ev.rect().width()+35)//36
+
+        # paint nucleotides
+        painter.save()
+        painter.translate(36*leftmost, 10)
+        for pos in range(leftmost, rightmost+1):
+            x = nucleotides[self.dna[pos]]
+            painter.save()
+            painter.translate(0, (self.counterDNA) * 2)
+            x.draw(painter, self.color_count, False)
+            painter.restore()
+            painter.save()
+            painter.translate(0, (self.counter - OUT)*2)
+            x.complement().draw(painter, 0, True)
+            painter.restore()
+            painter.translate(36, 0)
+        painter.restore()
+
+        # underline genes
+        for tata, tac, stop, _ in self.genes:
+            painter.setPen(QPen(QColor(140, 0, 0), 5, Qt.SolidLine))
+            painter.drawLine(tata*36,170, (tata+4)*36, 170)
+
+            painter.setPen(QPen(QColor(140, 0, 0), 5, Qt.DotLine))
+            painter.drawLine((tata+4)*36, 170, (tac)*36, 170)
+
+            painter.setPen(QPen(QColor(0, 140, 0), 5, Qt.SolidLine))
+            painter.drawLine((tac)*36, 170, stop*36, 170)
+            painter.drawLine((tac)*36, 160, tac *36, 170)
+            painter.drawLine(stop *36, 160, stop*36, 170)
+
+
     def paintEvent(self, ev):
-        dn = {'T': 'A','C': 'G','G': 'C','A' : 'T'}
         try:
             painter = QPainter(self)
-            painter.save()
-            leftmost = (ev.rect().x())//36
-            rightmost = (ev.rect().x() + ev.rect().width()+35)//36
-            painter.translate(36*leftmost, 10)
-
-
-            # find the leftmost gene that is on the screen
-
-            for pos in range(leftmost, rightmost+1):
-                x = self.dna[pos]
-                n = dn[x]
-                painter.save()
-                painter.translate(0, (self.counterDNA) * 2)
-                nucleotides[x].draw(painter, self.color_count, nucleotides[x].shape)
-                painter.restore()
-                painter.save()
-                painter.translate(0, (self.counter - OUT)*2)
-                nucleotides[n].draw(painter, 0, nucleotides[n].rshape)
-                painter.restore()
-                painter.translate(36, 0)
-
-            painter.restore()
-            for tata, tac, stop in self.genes:
-                painter.setPen(QPen(QColor(140, 0, 0), 5, Qt.SolidLine))
-                painter.drawLine(tata*36,170, (tata+4)*36, 170)
-
-                painter.setPen(QPen(QColor(140, 0, 0), 5, Qt.DotLine))
-                painter.drawLine((tata+4)*36, 170, (tac)*36, 170)
-
-                painter.setPen(QPen(QColor(0, 140, 0), 5, Qt.SolidLine))
-                painter.drawLine((tac)*36, 170, stop*36, 170)
-                painter.drawLine((tac)*36, 160, tac *36, 170)
-                painter.drawLine(stop *36, 160, stop*36, 170)
-
+            if self.mode == self.M_WHOLE:
+                self.paintWholeGenome(ev, painter)
+            elif self.mode == self.M_ZOOM:
+                self.paintZoom(ev, painter)
         except Exception as e:
-            print(e)
+            print('in paintEvent()', e)
 
     def start_trans(self):
         self.counter = 0
@@ -190,7 +253,7 @@ if __name__ == "__main__":
 
 
     app = QApplication(argv)
-    dna = DNA_view('AACCCCAATATACCTACAAAATT'*1000)        #    rl("dna_sequence")
+    dna = DNA_view('AACCCCAATATACCATGAAACCCGGGAAATAA'*3)        #    rl("dna_sequence")
 
     win = QScrollArea()
     win.setWidget(dna)
@@ -206,6 +269,11 @@ if __name__ == "__main__":
     slider3 = QSlider(Qt.Horizontal)
     slider3.setRange(0, 100)
     slider3.valueChanged.connect(dna.setCounterDNA)
+
+    slider4 = QSlider(Qt.Horizontal)
+    slider4.setRange(0, 100)
+    slider4.valueChanged.connect(dna.setZoom)
+
 
 
     button1 = QPushButton("Start transcription")
@@ -223,6 +291,7 @@ if __name__ == "__main__":
     layout.addWidget(slider1)
     layout.addWidget(slider2)
     layout.addWidget(slider3)
+    layout.addWidget(slider4)
     layout.addWidget(button1)
 
     panel = QWidget()
